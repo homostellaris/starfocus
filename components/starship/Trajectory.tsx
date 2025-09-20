6633d93fbbf901959d7161aa1dc769f08840e124
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2025-09-20 18:01:53 +0100

    Add keyboard metadata & opengraph route

--- components/starship/Trajectory.tsx
+++ components/starship/Trajectory.tsx
@@ -1,12 +1,21 @@
 import { cn } from '../common/cn'
 
-export default function Tracjectory(props: JSX.IntrinsicElements['div']) {
+export default function Tracjectory({
+	className,
+	orientation = 'vertical',
+}: {
+	orientation?: 'vertical' | 'horizontal'
+} & JSX.IntrinsicElements['div']) {
 	return (
 		<div
-			className={cn('trajectory h-full w-[2px] bg-supernova', props.className)}
+			className={cn(
+				'trajectory bg-supernova',
+				orientation === 'vertical' ? 'h-full w-[2px]' : 'w-full h-[2px]',
+				className,
+			)}
 			// Make the mask solid behind the rocket for a 'completed' effect
 			style={{
-				maskImage: 'repeating-linear-gradient(180deg, black, transparent 10px)',
+				maskImage: `repeating-linear-gradient(${orientation === 'vertical' ? '180' : '90'}deg, black, transparent 10px)`,
 				// maskSize: '10px 100%',
 			}}
 		></div>
