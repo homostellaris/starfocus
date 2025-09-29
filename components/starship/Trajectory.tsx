988e3b94a1812a7e6bd1006abcceb17745155833
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2025-09-29 22:16:23 +0100

    Translation method of trajectory progress

--- components/starship/Trajectory.tsx
+++ components/starship/Trajectory.tsx
@@ -9,18 +9,26 @@ export default function Tracjectory({
 	orientation?: 'vertical' | 'horizontal'
 } & JSX.IntrinsicElements['div']) {
 	return (
-		<div
-			className={cn(
-				'bg-[linear-gradient(theme(colors.rose.400),theme(colors.pink.400),theme(colors.fuchsia.400),theme(colors.violet.400),theme(colors.indigo.400),theme(colors.blue.400))]',
-				"[mask-image:repeating-linear-gradient(180deg,black,transparent_10px),url('/black-rectangle-svgrepo-com.svg')] [mask-repeat:no-repeat] [mask-position:left_top,left_top]",
-				orientation === 'vertical' ? 'h-full w-[2px]' : 'w-full h-[2px]',
-				className,
-			)}
-			// Make the mask solid behind the rocket for a 'completed' effect
-			style={{
-				// maskSize: `auto, 100px`,
-				animation: '4s linear 0s infinite alternate sun-rise',
-			}}
-		></div>
+		<>
+			<div
+				className={cn(
+					'bg-[linear-gradient(theme(colors.rose.400),theme(colors.pink.400),theme(colors.fuchsia.400),theme(colors.violet.400),theme(colors.indigo.400),theme(colors.blue.400))]',
+					"[mask-image:repeating-linear-gradient(180deg,black,transparent_10px),url('/black-rectangle-svgrepo-com.svg')] [mask-repeat:no-repeat] [mask-position:left_top,left_top]",
+					orientation === 'vertical' ? 'h-full w-[2px]' : 'w-full h-[2px]',
+					className,
+				)}
+			></div>
+			<div
+				className={cn(
+					'bg-[linear-gradient(theme(colors.rose.400),theme(colors.pink.400),theme(colors.fuchsia.400),theme(colors.violet.400),theme(colors.indigo.400),theme(colors.blue.400))]',
+					orientation === 'vertical' ? 'h-full w-[2px]' : 'w-full h-[2px]',
+					'transition-transform duration-500 ease-in-out',
+					className,
+				)}
+				style={{
+					transform: `translate(0, calc(calc(100% - ${currentPosition + 20}px) * -1))`,
+				}}
+			></div>
+		</>
 	)
 }
