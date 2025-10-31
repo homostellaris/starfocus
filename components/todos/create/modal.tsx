ba6d19982815dbed182598e09ad274acbdeaf578
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2025-10-31 22:38:41 +0000

    🐛 Editing not loading initial data
    In fixing this also made it more naturally handle moving todos from wayfinder to asteroid field or vice versa according to eligibility.

--- components/todos/create/modal.tsx
+++ components/todos/create/modal.tsx
@@ -1,66 +1,12 @@
-import { IonSelect, IonSelectOption } from '@ionic/react'
-import { ComponentProps, useRef } from 'react'
-import { ListType } from '../../db'
+import { ComponentProps } from 'react'
 import TodoModal from '../TodoModal'
 
 export function CreateTodoModal({
-	dismiss,
 	...props
-}: {
-	dismiss: (data?: any, role?: string) => void
-} & ComponentProps<typeof TodoModal>) {
-	const locationSelect = useRef<HTMLIonSelectElement>(null)
-
+}: {} & ComponentProps<typeof TodoModal>) {
 	return (
 		<TodoModal
 			id="create-todo-modal"
-			dismiss={(data?: any, role?: string) => {
-				dismiss(
-					{
-						todo: data,
-						location: locationSelect.current?.value,
-					},
-					role,
-				)
-			}}
-			onKeyDown={event => {
-				if (event.metaKey) {
-					locationSelect.current!.value = ListType.icebox
-				}
-			}}
-			onKeyUp={event => {
-				if (!event.metaKey) {
-					locationSelect.current!.value = ListType.asteroidField
-				}
-			}}
-			toolbarSlot={({ starPoints }) => {
-				const eligibleForWayfinder = !!starPoints
-				return (
-					<IonSelect
-						className="p-2"
-						fill="outline"
-						ref={locationSelect}
-						slot="end"
-						value={
-							eligibleForWayfinder ? ListType.wayfinder : ListType.asteroidField
-						}
-					>
-						<IonSelectOption value={ListType.icebox}>Icebox</IonSelectOption>
-						<IonSelectOption
-							disabled={!!eligibleForWayfinder}
-							value={ListType.asteroidField}
-						>
-							Asteroid Field
-						</IonSelectOption>
-						<IonSelectOption
-							disabled={!eligibleForWayfinder}
-							value={ListType.wayfinder}
-						>
-							Wayfinder
-						</IonSelectOption>
-					</IonSelect>
-				)
-			}}
 			{...props}
 		/>
 	)
